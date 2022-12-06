0ea3a686fcf05a4df6859174a0396f84f7a29877
mohamad-ridwan <mr643062@gmail.com>
2022-12-06 15:09:15 +0700

    ridwan1 hosting firebase with api cyclic.sh

--- src/services/api/endpoint.js
+++ src/services/api/endpoint.js
@@ -1,3 +1,4 @@
-const endpoint = 'https://bibit-rp.herokuapp.com'
+// const endpoint = 'https://bibit-rp.herokuapp.com'
+const endpoint = 'https://long-cow-jeans.cyclic.app'
 
 export default endpoint
